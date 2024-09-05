1f26f96814fbc1c0f1d22afbd52327b996aa3674
A-ri-SIM <149152400+A-ri-SIM@users.noreply.github.com>
2024-09-05 17:14:42 +0900

    size 수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { useState } from 'react';
-import './App.css';
 import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
 import { DarkModeProvider } from './context/DarkModeContext';
 import DateTime from './components/Date/DateTime';
-
 
 const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
     <DarkModeProvider>
-      <DateTime/>
+      <DateTime />
       <div className="box">
         <Header filters={filters} filter={filter} onFilterChange={setFilter} />
         <TodoList filter={filter} />
